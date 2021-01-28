20a1452202c3a7302f6e129d3ff5e10744fe6800
ardigermenji <ardi.germenji@gmail.com>
2021-01-28 10:26:31 +0100

    add spinner

--- app.js
+++ app.js
@@ -30,8 +30,10 @@ function calcutaResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    showLoading('.results');
   } else {
     showError('Please check your numbers');
+    // !pulire input!
   }
 }
 
@@ -49,6 +51,8 @@ function showError(error) {
   // isnerisco nel dom
   card.insertBefore(errorDiv, heading);
 
+  showLoading('.alert');
+
   // pulisco errore dopo un po'
   setTimeout(clearError(errorDiv), 3000);
 }
@@ -56,4 +60,23 @@ function showError(error) {
 // closure
 function clearError(err) {
   return () => err.remove();
+}
+
+// loading
+function showLoading(element) {
+  // nascondo la sezione resultper un tot
+  const resultsSection = document.querySelector(element);
+  resultsSection.classList.add('d-none');
+
+  // mostro lo spinner per poco
+  const spinner = document.querySelector('.spinner-grow');
+  spinner.classList.remove('d-none');
+
+  // dopo qualche sec inverto
+  setTimeout(() => {
+    resultsSection.classList.remove('d-none');
+    spinner.classList.add('d-none');
+  }, 1000);
+  // nascondo lo spinner
+  // mostro sezione
 }
